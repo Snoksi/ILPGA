4ba4468ab0b9a24fb684e28c559c4ac0f986add4
Brendan <brendan.poirier@hetic.net>
2018-04-11 12:00:11 +0200

    Start parsing stimulus

--- src/Service/Uploader/StimulusUploader.php
+++ src/Service/Uploader/StimulusUploader.php
@@ -5,6 +5,8 @@ namespace App\Service\Uploader;
 use App\Entity\Stimulus;
 use App\Entity\Test;
 use Doctrine\ORM\Mapping\Entity;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class StimulusUploader extends FileUploader
@@ -26,8 +28,44 @@ class StimulusUploader extends FileUploader
         }
     }
 
-    public function bind($excel){
+    public function bind(UploadedFile $excel){
+        $excel->getRealPath();
 
+        $inputFileType = IOFactory::identify($excel);
+        /**  Create a new Reader of the type that has been identified  **/
+        $reader = IOFactory::createReader($inputFileType);
+        /**  Load $inputFileName to a Spreadsheet Object  **/
+        $spreadsheet = $reader->load($excel);
+
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $row = 2;
+        while($finished === true){
+            $stimulusName = $sheet->getCell($col."A")->getValue();
+            if("" != $stimulus = $this->stimuli[$stimulusName]){
+                exit;
+            }
+
+            $stimulus->setAge()
+
+            $row++;
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $test_table = $em->getRepository("App:Stimulus")->findOneBy([
+            'stimulus' => $name,
+            'test' => $test,
+        ]);
+        if (!empty($test_table)) {
+            $test_table->setAge($spreadsheet->getActiveSheet()->getCell('C2'));
+            $test_table->setSexe($spreadsheet->getActiveSheet()->getCell('D2'));
+            $test_table->setLangue($spreadsheet->getActiveSheet()->getCell('E2'));
+            $test_table->setQuestion($spreadsheet->getActiveSheet()->getCell('G2'));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($test_table);
+            $em->flush();
+            echo 'bravo';
+        }else { echo 'vide';}
     }
 
     public function getStimuli()
