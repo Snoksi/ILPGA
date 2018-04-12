61c47b8b82fcc6b2cbf0284876568b6fb38ca2e7
Grumeleux <romain.peter3@gmail.com>
2018-04-12 23:12:27 +0200

    Front end step 1 2

--- src/Form/CreateTestType.php
+++ src/Form/CreateTestType.php
@@ -7,15 +7,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 
 class CreateTestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('notified')
-            ->add('instructionPage', InstructionPageType::class)
+            ->add('name', TextType::class, ['label' => 'Nom du test'])
+            ->add('notified', CheckboxType::class, ['label' => 'Être notifié des réponses'])
+            ->add('instructionPage', InstructionPageType::class, ['label' => 'Instructions générales'])
         ;
     }
 
