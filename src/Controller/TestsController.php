9126749235ada310223ce91295228bcfa62489a2
tayeb saidi <saidi.tayeb@hotmail.com>
2018-04-12 15:15:20 +0200

    update testcontroller and add service testmanager

--- src/Controller/TestsController.php
+++ src/Controller/TestsController.php
@@ -1,15 +1,24 @@
 <?php
-
 namespace App\Controller;
-
+use App\Entity\Page;
 use App\Entity\Test;
 use App\Entity\TestFolder;
+use App\Form\CreatePostTestFormType;
+use App\Form\CreateTestType;
+use App\Form\StimuliAndQuestionsFormType;
+use App\Service\TestManager;
+use App\Service\Uploader\ExcelParser;
+use App\Service\Uploader\FileUploader;
+use App\Service\Uploader\StimulusUploader;
+use App\Utils\FormGenerator\PreTestFormSerializer;
+use Doctrine\ORM\Mapping\Entity;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 /**
  * Class TestsController
  * @package App\Controller
@@ -20,6 +29,8 @@ class TestsController extends Controller
     /**
      * @Route("/{id}", name="tests_index", requirements={"id"="\d*"})
      * @ParamConverter("test_folder", class="App:TestFolder", isOptional=true)
+     * @param TestFolder|null $folder
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index(TestFolder $folder = null)
     {
@@ -28,19 +39,15 @@ class TestsController extends Controller
             $em = $this->getDoctrine()->getManager();
             $folders = $em->getRepository('App:TestFolder')->findRootFolders();
             $tests = $em->getRepository('App:Test')->findRootTests();
-
             $folder = new TestFolder();
             $folder->setName("/");
             $folder->setChildren($folders);
             $folder->setTests($tests);
         }
-
         return $this->render('tests/index.html.twig', [
             'current_folder' => $folder
         ]);
     }
-
-
     /**
      * @Route("/search/{query}", name="tests_search")
      * @param $query
@@ -50,11 +57,112 @@ class TestsController extends Controller
         $em = $this->getDoctrine()->getManager();
         $folders = $em->getRepository("App:TestFolder")->findByName($query);
         $tests = $em->getRepository("App:Test")->findByName($query);
-
         return $this->render('tests/search.html.twig', [
             'query' => $query,
             'folders' => $folders,
             'tests' => $tests
         ]);
+    }
+    /**
+     * @Route("/create", name="tests_create")
+     * @Route("/create/in_folder/{id}", name="tests_create_in")
+     * @Route("/create/step/{step}", name="tests_create_step")
+     * @ParamConverter(name="folder", class="App:TestFolder", isOptional=false)
+     * @param SessionInterface $session
+     * @param TestFolder|null $folder
+     * @param int $step
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function create(SessionInterface $session, TestFolder $folder = null, $step = 1)
+    {
+        $reachedStep = $session->get('reached_step', 1);
+        if($step > $reachedStep){
+            throw new AccessDeniedException();
+        }
+        if($step == 1){
+            return $this->forward("App\Controller\TestsController::step1", [
+                'folder' => $folder
+            ]);
+        }
+        $testId = $session->get('editing_test_id');
+        $em = $this->getDoctrine()->getManager();
+        $test = $em->getRepository('App:Test')->find($testId);
+        return $this->forward("App\Controller\TestsController::step".$step, [
+            'test' => $test
+        ]);
+    }
+    /**
+     * @param Request $request
+     * @param TestFolder|null $folder
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function step1(Request $request, SessionInterface $session, TestFolder $folder = null)
+    {
+        $test = new Test();
+        $page = new Page();
+        $page->setTitle("Page d'instructions");
+        $page->setType("page");
+        $page->setTest($test);
+        $test->setFolder($folder);
+        $test->setInstructionPage($page);
+        $form = $this->createForm(CreateTestType::class, $test);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($test);
+            $em->flush();
+            $session->set('editing_test_id', $test->getId());
+            return $this->redirectToStep(2);
+        }
+        return $this->render('tests/step_1.html.twig', ['form' => $form->createView()]);
+    }
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function step2(Request $request, SessionInterface $session, Test $test){
+        $form = $this->createForm(CreatePostTestFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $formSerializer = new PreTestFormSerializer($form->getData());
+            $page = new Page();
+            $page->setTitle("Formulaire pre-test");
+            $page->setType('form');
+            $page->setContent($formSerializer->getSerializedForm());
+            $page->setTest($test);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            $em->flush();
+            return $this->redirectToStep(3);
+        }
+        return $this->render('tests/step_2.html.twig', ['form' => $form->createView()]);
+    }
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function step3(Request $request, SessionInterface $session, TestManager $testManager, Test $test){
+        $form = $this->createForm(StimuliAndQuestionsFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $input = $form->getData();
+            $testManager->setTest($test);
+            $testManager->addStimuli($input['audio']);
+            $testManager->bindExcel($input['excel']);
+            $questions = $testManager->getQuestions();
+            $em = $this->getDoctrine()->getManager();
+            foreach($questions as $question){
+                $em->persist($question);
+            }
+            $em->flush();
+        }
+        return $this->render('tests/step_3.html.twig', ['form' => $form->createView()]);
+    }
+    public function redirectToStep($step = 1){
+        $this->get('session')->set('reached_step', $step);
+        return $this->redirectToRoute('tests_create_step', ['step' => $step]);
     }
 }
---
+++ src/Service/TestManager.php
@@ -0,0 +1,152 @@
+<?php
+namespace App\Service;
+use App\Entity\Page;
+use App\Entity\Stimulus;
+use App\Entity\Test;
+use App\Service\Uploader\FileUploader;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Intl\Exception\MissingResourceException;
+class TestManager
+{
+    protected $test;
+    protected $stimuli = [];
+    protected $questions = [];
+    protected $lastQuestion;
+    protected $uploader;
+    public function __construct(FileUploader $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+    /**
+     * @param Test $test
+     */
+    public function setTest(Test $test)
+    {
+        $this->test = $test;
+    }
+    /**
+     * @return mixed
+     */
+    public function getTest()
+    {
+        return $this->test;
+    }
+    /**
+     * Uploads all mp3/mp4 files to a public folder in the test folder
+     * @param $files
+     */
+    public function addStimuli($files){
+        if(!$this->test instanceof Test){
+            throw new MissingResourceException("App\Entity\Test is missing");
+        }
+        $this->uploader->setTargetDirectory("/uploads/tests/".$this->test->getId());
+        foreach($files as $file){
+            $stimulus = new Stimulus();
+            $stimulus->setName($file->getClientOriginalName());
+            $fileName = $this->uploader->upload($file);
+            $stimulus->setSource($this->uploader->getTargetDirectory()."/".$fileName);
+            $this->stimuli[$file->getClientOriginalName()] = $stimulus;
+        }
+    }
+    /**
+     *  Read excel and bind informations to the correct stimulus
+     */
+    public function bindExcel(UploadedFile $excel){
+        $sheet = $this->getSheetReader($excel);
+        $row = 2;
+        while(true) {
+            $stimulusName = $sheet->getCell("A" . $row)->getValue();
+            // If there is no stimulus, stops the parsing
+            if (!isset($this->stimuli[$stimulusName]) && $stimulusName == "") break;
+            // Else we fill the stimulus of informations
+            $stimulus = $this->stimuli[$stimulusName];
+            $stimulus->setSpeakerAge($sheet->getCell("B" . $row)->getValue());
+            $stimulus->setSpeakerGender($sheet->getCell("C" . $row)->getValue());
+            $stimulus->setSpeakerLang($sheet->getCell("D" . $row)->getValue());
+            $stimulus->setPlayCount($sheet->getCell("E" . $row)->getValue());
+            $question = $this->generateQuestionPage($row, $sheet);
+            $question->addStimulus($stimulus);
+            $this->questions[] = $question;
+            $row++;
+        }
+    }
+    public function getQuestions()
+    {
+        return $this->questions;
+    }
+    private function getType($type)
+    {
+        switch($type)
+        {
+            case "choix_multiple":
+                return "checkbox";
+            case "choix_unique":
+                return "radio";
+            case "nombre":
+                return "number";
+            case "range":
+                return "range";
+            default:
+                return "text";
+        }
+    }
+    private function generateQuestionPage($row, $sheet)
+    {
+        // Si aucune question n'a été associé à ce stimulus, alors on l'associe à la question précédente
+        if($sheet->getCell("F".$row)->getValue() == "") return $this->lastQuestion;
+        $question = new Page();
+        $question->setTest($this->getTest());
+        $question->setType("question");
+        $question->setTitle($sheet->getCell("F" . $row)->getValue());
+        $question->setContent($this->generateQuestionContent($row, $sheet));
+        $question->lastQuestion = $question;
+        return $question;
+    }
+    private function generateQuestionContent($row, $sheet)
+    {
+        $content = [
+            "label" => $sheet->getCell("F" . $row)->getValue(),
+            "type" => $this->getType($sheet->getCell("G" . $row)->getValue()),
+        ];
+        if ($content['type'] == "checkbox" || $content['type'] == "radio")
+        {
+            $choices = [];
+            $col = "H";
+            $has_choices = true;
+            while ($has_choices) {
+                $choice = $sheet->getCell($col . $row)->getValue();
+                if ($choice == "") exit;
+                $choices[$choice] = $choice;
+                $col++;
+            }
+            $content["choices"] = $choices;
+        }
+        if($content['type'] == "range")
+        {
+            $content['min'] = $sheet->getCell("H".$row)->getValue();
+            $content['max'] = $sheet->getCell("I".$row)->getValue();
+        }
+        return $content;
+    }
+    /**
+     * @param $excel
+     * @return \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     */
+    private function getSheetReader($excel){
+        try{
+            $inputFileType = IOFactory::identify($excel->getRealPath());
+            /**  Create a new Reader of the type that has been identified  **/
+            $reader = IOFactory::createReader($inputFileType);
+            /**  Load $inputFileName to a Spreadsheet Object  **/
+            $spreadsheet = $reader->load($excel);
+        }
+        catch(\Exception $exception){
+            throw new BadRequestHttpException("Inccorrect Excel file");
+        }
+        return $spreadsheet->getActiveSheet();
+    }
+}
